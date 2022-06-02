0aa6ffa4c2735ecdca06ecc853d3fdbe6dcdd258
arnaldoriofrio <62032451+arnaldoriofrio@users.noreply.github.com>
2022-06-02 08:42:21 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://7c7dfa81.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
